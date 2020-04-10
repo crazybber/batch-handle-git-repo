d1b42de99009fde6ed4a279d85bfb081fc827d7f
Edward <chenlulei@gmail.com>
2020-04-10 13:53:33 +0800

    Create rm-all-nul.sh

---
+++ rm-all-nul.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+errout=">> syncLog.txt 2>&1"
+echo ------------------------------------
+#echo "errout: ${errout}"
+
+index_count=0
+
+time=$(date "+%Y-%m-%d %H:%M:%S")
+
+filename=nul
+
+echo "###########${time} Begin Del ${filename}"
+
+read -n1 -p "Press any key to continue..."
+
+function start_clean_all_nul(){
+for dir in $(ls -d *)
+do
+  if [ -f "$dir"/"${filename}" ]; then
+	cd $dir
+	echo "${time} Change to: ${PWD}"
+    echo "${time} Begin to del nul: ${dir}"
+	rm nul
+	echo "${time} End in Repo: ${dir}"
+    cd ..
+	echo "${time} Return to: ${PWD}"
+ 
+  fi
+done
+
+echo "###########Dir Count : ${#code_repo_list[@]}, deleted: ${index_count}."
+}
+
+
+start_clean_all_nul
+
+time=$(date "+%Y-%m-%d %H:%M:%S")	
+echo "###########${time} End Del "
+
+
+read -n1 -p "Press any key to continue..."
